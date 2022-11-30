e0c7de2cc7156dd6af194b61161bf244d0ab6f53
Daria <macdaria1999@gmail.com>
2022-11-30 21:42:03 +0300

    Add task 13

---
+++ task_13/Program.cs
@@ -0,0 +1,7 @@
+﻿// task13
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>99)
+    Console.WriteLine($"Третье число: {a%10}");
+else
+    Console.WriteLine("Третьей цифры нет");
