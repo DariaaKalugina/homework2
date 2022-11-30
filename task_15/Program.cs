f7cfd57d0a1d8d3bfc5102c7fce020e8462b75e0
Daria <macdaria1999@gmail.com>
2022-11-30 22:07:40 +0300

    Add task15

---
+++ task_15/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// task15
+Console.Write("Введите цифру, обозначающую день недели: ");
+int d = Convert.ToInt32(Console.ReadLine());
+while (d < 1 || d > 7)
+{
+    Console.Write("Вы ошиблись! Введите день недели: ");
+    d = Convert.ToInt32(Console.ReadLine());
+}
+if (d==6 || d==7)
+    Console.Write("Это выходной!");
+else
+    Console.Write("Это не выходной!");
